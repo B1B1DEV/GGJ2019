6fea1951eafa5cef3ad4424469edd0b385cebbc5
Olivier <ochabiron@immersivefactory.com>
2019-01-26 19:08:26 +0100

    erase renderer destination + fog

--- Assets/Scripts/DestinationController.cs
+++ Assets/Scripts/DestinationController.cs
@@ -6,7 +6,7 @@ public class DestinationController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        GetComponent<Renderer>().enabled = false;
 	}
 	
 	// Update is called once per frame
