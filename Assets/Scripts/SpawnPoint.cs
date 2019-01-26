6829f6ed4c42e7d4a93679c3009917e9a238b354
Olivier <ochabiron@immersivefactory.com>
2019-01-26 23:06:32 +0100

    spawn small obstacles system + bake reflection probes

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SpawnPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+
+}
---
+++ Assets/Scripts/SpawnProp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProp : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject propsPool;
+
+    private List<GameObject> propsList;
+    private List<Transform> zoneList;
+
+    // Use this for initialization
+    void Start ()
+    {
+        BuildPropsList();
+        BuildZoneList();
+
+        // Affectation
+        Affect();
+    }
+	
+
+    // Build list
+    void BuildPropsList()
+    {
+        propsList = new List<GameObject>(); 
+
+        for (int i = 0; i < propsPool.transform.childCount; i++)
+        {
+            propsList.Add(propsPool.transform.GetChild(i).gameObject);
+        }
+    }
+
+    void BuildZoneList()
+    {
+        zoneList = new List<Transform>();
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            zoneList.Add(this.transform.GetChild(i));
+        }
+    }
+
+    // randomly picks an object from the pool
+    void Affect()
+    {
+        foreach (Transform zone in zoneList)
+        {
+            // Random pick
+            int rn = Random.Range(0, propsList.Count-1);
+            //propsList[rn].transform.SetParent(this.transform);
+            propsList[rn].transform.SetPositionAndRotation(zone.transform.position,zone.transform.rotation);
+
+            if (propsList.Count>1)
+                propsList.RemoveAt(rn);
+        }
+    }
+}
