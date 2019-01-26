71d90fd46a04ef25928a2c8c293c414b6733e6f5
Remi minitop <remi@uselessworks.com>
2019-01-26 10:42:26 +0100

    Car

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputController : MonoBehaviour
+{
+    public abstract float getWheelInput();
+    public abstract Vector2 getHeadInput();
+    public abstract bool isBreaking();
+}
