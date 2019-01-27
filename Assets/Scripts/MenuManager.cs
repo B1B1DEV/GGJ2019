c7274850a6f0fa8d7173edadbe55255b01ff6295
LeBanc <ben.ledirach@gmail.com>
2019-01-27 16:02:47 +0100

    Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using AudioToolkit;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioManager.Instance.Play("TechnoCrapMenu");
+        AudioManager.Instance.Play("BrouhahaMenu");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.anyKeyDown && !m_oneTime)
+        {
+            m_oneTime = true;
+            StartCoroutine(SwitchLevel());
+        }
+	}
+
+    bool m_oneTime = false;
+
+    IEnumerator SwitchLevel()
+    {
+        AudioManager.Instance.Stop("TechnoCrapMenu");
+        AudioManager.Instance.Stop("BrouhahaMenu");
+
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("Level");
+    }
+}
